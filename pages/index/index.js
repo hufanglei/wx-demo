d54495564a9649d934f62ebb4d1674633c374f98
hfl <690328661@qq.com>
2020-06-21 19:26:02 +0800

    init

---
+++ pages/index/index.js
@@ -0,0 +1,16 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  //为页面提供数据的
+  //data就是界面和逻辑之间的桥梁
+  data: {
+    message: 'Hello World',
+    person:{
+      name:"小胡"
+    },
+    viewClassName: 'hello'
+  }
+
+})
